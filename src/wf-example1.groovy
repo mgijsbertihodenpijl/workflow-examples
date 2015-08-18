cec805b9e3bc629863b836472c54a3666a2b7021
Martijn Gijsberti Hodenpijl <mgijsbertihodenpijl@schubergphilis.com>
2015-08-18 16:27:00 +0200

    Example 1

---
+++ src/wf-example1.groovy
@@ -0,0 +1,12 @@
+def flow = '''node {
+  git url: 'https://github.com/jglick/simple-maven-project-with-tests.git'
+  def mvnHome = tool 'M3'
+  sh "${mvnHome}/bin/mvn -B verify"
+}'''
+workflowJob('wf-1') {
+    definition {
+        cps {
+            script(flow)
+        }
+    }
+}
